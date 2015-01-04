b42778ce32307bf4916d26440ac463ff8d095bbb
lm <mueller.lutz@gmail.com>
2015-01-04 02:42:52 +0100

    persist all of a persons attributes

--- src/main/java/io/github/lumue/examples/sbetl/PersonWriter.java
+++ src/main/java/io/github/lumue/examples/sbetl/PersonWriter.java
@@ -6,6 +6,6 @@ import org.springframework.batch.item.database.JdbcBatchItemWriter;
 public class PersonWriter extends JdbcBatchItemWriter<Person> {
 	{
 		setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Person>());
-		setSql("INSERT INTO person (first_name, last_name) VALUES (:firstName, :lastName)");
+		setSql("INSERT INTO person (first_name, last_name,street,city,country,email) VALUES (:firstName, :lastName,:street,:city,:country,:email)");
 	}
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,7 @@
-create table person(first_name varchar(256),last_name varchar(256));
+create table person(	first_name varchar(256),
+				last_name varchar(256),
+				street varchar(256),
+				city varchar(256),
+				country varchar(256),
+				email varchar(256)
+				);
