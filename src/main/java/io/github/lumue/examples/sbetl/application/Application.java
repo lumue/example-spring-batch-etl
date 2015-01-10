f64e96375a322b9eb1c36d6fcefb8852da994407
lm <mueller.lutz@gmail.com>
2015-01-10 21:37:29 +0100

    start job on fileupload

--- src/main/java/io/github/lumue/examples/sbetl/application/Application.java
+++ src/main/java/io/github/lumue/examples/sbetl/application/Application.java
@@ -1,21 +1,49 @@
 package io.github.lumue.examples.sbetl.application;
 
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableAutoConfiguration
 @EnableBatchProcessing
 @EnableTransactionManagement
-@ImportResource("classpath:io/github/lumue/examples/sbetl/job/*.xml")
-public class Application {
+@ImportResource({ "classpath:io/github/lumue/examples/sbetl/job/*.xml", "classpath:io/github/lumue/examples/sbetl/application/*.xml" })
+@ComponentScan(basePackages = "io.github.lumue")
+public class Application extends SpringApplication {
+
+	@Bean
+	@Value("${application.upload.path}")
+	Path fileUploadPath(String uploadPath) throws IOException {
+		Path path = Files.createDirectories(Paths.get(uploadPath));
+		return path;
+	}
+
+	@Bean
+	public JobLauncher jobLauncher(JobRepository jobRepository) {
+		SimpleJobLauncher simpleJobLauncher = new SimpleJobLauncher();
+		simpleJobLauncher.setJobRepository(jobRepository);
+		simpleJobLauncher.setTaskExecutor(new SimpleAsyncTaskExecutor());
+		return simpleJobLauncher;
+	}
 
 
 	public static void main(String[] args) throws Exception {
-		System.exit(SpringApplication.exit(SpringApplication.run(
-				Application.class, args)));
+		SpringApplication.run(
+				Application.class, args);
 	}
 }
---
+++ src/main/java/io/github/lumue/examples/sbetl/application/UploadController.java
@@ -0,0 +1,51 @@
+package io.github.lumue.examples.sbetl.application;
+
+import java.io.BufferedOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class UploadController {
+
+	@Autowired
+	Path fileUploadPath;
+
+	@RequestMapping(value = "/upload", method = RequestMethod.GET)
+	public @ResponseBody String provideUploadInfo() {
+		return "You can upload a file by posting to this same URL.";
+	}
+
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	public @ResponseBody String handleFileUpload(@RequestParam("file") MultipartFile file) {
+		if (!file.isEmpty()) {
+			try {
+				byte[] bytes = file.getBytes();
+				
+				String filename = UUID.randomUUID().toString();
+				Path targetPath = Files.createFile(fileUploadPath.resolve(filename + ".complete"));
+				Path tempPath = Files.createFile(fileUploadPath.resolve(filename + ".part"));
+				
+				BufferedOutputStream stream =
+						new BufferedOutputStream(Files.newOutputStream(targetPath));
+				stream.write(bytes);
+				stream.close();
+				
+				Files.move(tempPath, targetPath);
+
+				return "success";
+			} catch (Exception e) {
+				return e.getMessage();
+			}
+		}
+		return "success";
+	}
+}
